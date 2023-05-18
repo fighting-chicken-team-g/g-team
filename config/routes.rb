aae431976656d014ebaf24d2b47dad43b96800e1
Kazuya Nagamachi <128442256+kazuya-na@users.noreply.github.com>
2023-05-18 07:44:15 +0000

    [コンフリクト]修正

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,38 @@ Rails.application.routes.draw do
   end
   devise_for :admins
   devise_for :end_users
+
+  namespace :public do
+    get 'deliveries/index'
+    get 'deliveries/edit'
+  end
+  namespace :public do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'end_users/show'
+    get 'end_users/edit'
+  end
+  get 'end_users/show'
+  get 'end_users/edit'
+  namespace :public do
+    get 'carts/index'
+  end
+  namespace :public do
+    get 'foods/index'
+    get 'foods/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+  devise_for :end_users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
