a673d4aec6832d5feda0383aa14aad9e2366734c
yutadayone <129708410+yutadayone@users.noreply.github.com>
2023-05-18 07:25:46 +0000

    [Finish]admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'end_users/index'
+    get 'end_users/show'
+    get 'end_users/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'foods/index'
+    get 'foods/new'
+    get 'foods/show'
+    get 'foods/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :admins
   devise_for :end_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
