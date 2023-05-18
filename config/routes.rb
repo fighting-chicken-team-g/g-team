4834a7b14b6d8d9679bf9ccf5857e23d40e35761
Kazuya Nagamachi <128442256+kazuya-na@users.noreply.github.com>
2023-05-18 07:23:06 +0000

    [Finish]controllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'deliveries/index'
+    get 'deliveries/edit'
+  end
+  namespace :public do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'end_users/show'
+    get 'end_users/edit'
+  end
+  get 'end_users/show'
+  get 'end_users/edit'
+  namespace :public do
+    get 'carts/index'
+  end
+  namespace :public do
+    get 'foods/index'
+    get 'foods/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
