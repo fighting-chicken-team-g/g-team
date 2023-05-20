b3e45ef838ecd353d166011f4d36e41d93ec53fb
Kazuya Nagamachi <128442256+kazuya-na@users.noreply.github.com>
2023-05-20 11:30:04 +0000

    [Start]カート内商品/index

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -1,4 +1,11 @@
 class Public::CartsController < ApplicationController
   def index
   end
+
+  def destroy_all
+    user = User.find(params[:id])
+    user.carts.destroy_all
+    Cart.destroy_all
+    redirect_to carts_path
+  end
 end
