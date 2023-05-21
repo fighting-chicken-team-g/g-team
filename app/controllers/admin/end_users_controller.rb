4fe4e1cf2643d2938b9bbda2c8ff2b63e8b045f1
yutadayone <129708410+yutadayone@users.noreply.github.com>
2023-05-21 12:51:22 +0000

    [Start]kokyaku index,show完

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,10 +1,24 @@
 class Admin::EndUsersController < ApplicationController
   def index
+    #ページネーション
+    #@end_users = EndUser.all
+    @end_users = EndUser.page(params[:page])
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+
+  end
+
+  private
+  # ストロングパラメータ
+  def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :last_name_read, :first_name_read, :post_code, :address, :tel, :email)
   end
 end
