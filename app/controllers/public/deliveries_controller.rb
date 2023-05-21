829f49ff0b1aaa3f4879e536591472a1e5a28ee4
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-05-21 05:20:13 +0000

    [途中]delivery

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,7 +1,45 @@
 class Public::DeliveriesController < ApplicationController
   def index
+    @deliveries = Delivery.all
   end
 
   def edit
+     #@deliveries = Delivery.all
+     @delivery = Delivery.find(params[:id])
+  end
+
+  def create
+     @deliveries = Delivery.new(delivery_params)
+     if @deliveries.save
+      redirect_to edit_delivery_path(@deliveries)
+     end
+  end
+
+  def update
+    @deliveries = Delivery.find(params[:id])
+    if @delivery.update(delivery_params)
+      redirect_to delivery_path(@book.id)
+    else
+      render "edit"
+    end
+   
+  end
+
+  def destroy
+    @deliveries = end_user.find_by(end_user_id: @deliveries.id)
+    deliveries.destroy
+    redirect_to request.referer
   end
 end
+
+ private
+
+  def delivery_params
+    params.require(:delivery).permit(:end_user_id, :post_code, :address, :delivery_name)
+  end
+
+
+
+
+
+
