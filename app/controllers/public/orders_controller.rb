6d05ed3bd2eb81e022b77d662f59251c111b8b80
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-05-22 08:39:04 +0000

    [注文詳細途中計算式から]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,27 +1,29 @@
 class Public::OrdersController < ApplicationController
   #before_action :authenticate_customer!
-  
+
   def index
-    @orders = current_end_user.orders.latest
+
+    @orders = Order.all
   end
 
   def show
-    @orders = current_end_user.orders.find(params[:id])
+    #@orders = current_end_user.orders.find(params[:id])
   end
-  
+
   def new
     @order = Order.new
-    @ordrs = current_end_users.all
+    @orders = current_end_user.orders
     #@client = Client.find(current_end_user.id)
-    #@delivery = @end_users_delivery
-  end 
-  
+    @deliveries = current_end_user.deliveries
+  end
+
   def confirm
-    @cart_foods = current_end_user.cart_foods
+    @cart_foods = current_end_user.cart_ids
     @total = 0
     @order = Order.new(order_params)
-    @order.is_order = 0    #注文のステータスが0#
+    #@order.is_order = 0    #注文のステータスが0#
     @delivery_price = 800 #送料円#
+    @total_price = Total_price
     if params[:order][:select_address] == "0"
             @order.post_code = current_end_user.post_code
             @order.address = current_end_user.address
@@ -32,12 +34,13 @@ class Public::OrdersController < ApplicationController
             @order.address = @address.address
             @order.name = @address.name
         else params[:order][:select_address] == "2"
+            
     end
   end
-  
+
   def completed
   end
-  
+
   def create
     @order = Order.new(order_params)
     @deliveries_price = 800
@@ -61,6 +64,6 @@ class Public::OrdersController < ApplicationController
 private
 
   def order_params
-    params.require(:order).permit(:payment, :address, :total_price, :post_code, :end_user_id, :delivery_price)
+    params.require(:order).permit(:payment, :address, :total_price, :post_code, :end_user_id, :delivery_price, :confirm, :name)
   end
 end
