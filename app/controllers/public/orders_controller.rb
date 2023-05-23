4b9c587d6e6f2b42978f06a17bb36297939dc7d8
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-05-23 01:56:01 +0000

    [オーダー途中]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,7 +7,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    #@orders = current_end_user.orders.find(params[:id])
+    @orders = current_end_user.orders.find(params[:id])
   end
 
   def new
@@ -18,43 +18,41 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @cart_foods = current_end_user.cart_ids
-    @total = 0
+    @cart_foods = current_end_user.carts.all
+    @order.delivery_price = 800 # 送料円 #
+    @total = @cart_foods.inject(0) { |sum, food| sum + order.subtota
     @order = Order.new(order_params)
-    #@order.is_order = 0    #注文のステータスが0#
-    @delivery_price = 800 #送料円#
     @total_price = Total_price
     if params[:order][:select_address] == "0"
-            @order.post_code = current_end_user.post_code
-            @order.address = current_end_user.address
-            @order.name = current_end_user.last_name + current_end_user.first_name
-        elsif params[:order][:select_address] == "1"
-            @address = Address.find(params[:order][:address_id])
-    　　      @order.post_code = @address.post_code
-            @order.address = @address.address
-            @order.name = @address.name
-        else params[:order][:select_address] == "2"
-            
+       @order.post_code = current_end_user.post_code
+       @order.address = current_end_user.address
+       @order.name = current_end_user.last_name + current_end_user.first_name
+    elsif params[:order][:select_address] == "1"
+       @address = Address.find(params[:order][:address_id])
+       @order.post_code = @address.post_code
+       @order.address = @address.address
+       @order.name = @address.name
+    else params[:order][:select_address] == "2"# @order.is_order = 0 # 注文のステータスが0 #
     end
-  end
+　end
 
   def completed
   end
 
   def create
     @order = Order.new(order_params)
-    @deliveries_price = 800
-    @order.is_order = 0
+    @delivery_price = 800
+    #@order.is_order = 0
     @order.save
     @cart_foods = current_end_user.cart_foods
     @cart_foods.each do |cart_food|
-    @order_detail = OrderDetail.new
-    @order_detail.order_id = @order.id
-    @order_detail.food_id = cart_food.food_id
-    @order_detail.tax_free_price = cart_food.tax_free_price
-    @order_detail.tax_free_price = cart_food.subtotal
-    @order_detail.is_making = 0
-    @order_detail.save
+    @order_status = Orderstatus.new
+    @order_status.order_id = @order.id
+    @order_status.food_id = cart_food.food_id
+    @order_status.tax_price = cart_food.tax_price
+    @order_status.tax_price = cart_food.subtotal
+    @order_status.is_making = 0
+    @order_status.save
     end
     current_customer.cart_items.destroy_all
     redirect_to orders_confirm_path
@@ -67,3 +65,14 @@ private
     params.require(:order).permit(:payment, :address, :total_price, :post_code, :end_user_id, :delivery_price, :confirm, :name)
   end
 end
+
+
+
+
+
+
+
+
+
+
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,3 +2,7 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :end_user
 end
+
+def subtotal
+   food.add_tax_sales_price * order_detail_count
+end
